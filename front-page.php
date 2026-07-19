c43385e29f622bb695065e429a43a6edaedc3204
Your Name <you@example.com>
2026-07-19 16:43:02 +0200

    change structure

--- front-page.php
+++ front-page.php
@@ -3,8 +3,8 @@
 ?>
 <?php get_header(); ?>
 
-<?php get_template_part('template-parts/page-templates/hero'); ?>
-<?php get_template_part('template-parts/page-templates/releases'); ?>
-<?php get_template_part('template-parts/page-templates/news'); ?>
+<?php get_template_part('template-parts/section-templates/hero'); ?>
+<?php get_template_part('template-parts/section-templates/releases'); ?>
+<?php get_template_part('template-parts/section-templates/news'); ?>
 
 <?php get_footer(); ?>
